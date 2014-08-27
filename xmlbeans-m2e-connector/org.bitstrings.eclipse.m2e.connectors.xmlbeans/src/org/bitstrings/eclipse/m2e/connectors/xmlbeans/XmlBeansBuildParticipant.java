bd34d09351016ee3f3ce221cc954288b6b4beefb
Andrew Taylor <andrew@brekka.org>
2014-08-27 20:37:03 +0000

    Include the generated classes sub path 'schemaorg_apache_xmlbeans' as a source folder so that its contents get included in target/classes. This addresses an issue where incremental updates would leave the XmlBeans type system out-of-sync with the classes (because changes to schemaorg_apache_xmlbeans were not 'compiled' across). We often refer to this as the XmlAnyType problem.

--- xmlbeans-m2e-connector/org.bitstrings.eclipse.m2e.connectors.xmlbeans/src/org/bitstrings/eclipse/m2e/connectors/xmlbeans/XmlBeansBuildParticipant.java
+++ xmlbeans-m2e-connector/org.bitstrings.eclipse.m2e.connectors.xmlbeans/src/org/bitstrings/eclipse/m2e/connectors/xmlbeans/XmlBeansBuildParticipant.java
@@ -11,9 +11,10 @@
 package org.bitstrings.eclipse.m2e.connectors.xmlbeans;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecution;
 import org.bitstrings.eclipse.m2e.common.BuildHelper;
@@ -21,69 +22,41 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.embedder.IMaven;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
-public class XmlBeansBuildParticipant extends MojoExecutionBuildParticipant
-{
-    public XmlBeansBuildParticipant(MojoExecution execution)
-    {
+public class XmlBeansBuildParticipant extends MojoExecutionBuildParticipant {
+    private IMavenProjectFacade projectFacade;
+
+    public XmlBeansBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution) {
         super(execution, true);
+        this.projectFacade = projectFacade;
     }
 
     @Override
-    public Set<IProject> build(int kind, IProgressMonitor monitor) throws Exception
-    {
+    public Set<IProject> build(int kind, IProgressMonitor monitor) throws Exception {
         final IMaven maven = MavenPlugin.getMaven();
         final BuildContext buildContext = getBuildContext();
         final MavenSession mavenSession = getSession();
         final MojoExecution mojoExecution = getMojoExecution();
 
-
-        if (ArrayUtils.isEmpty(
-                    BuildHelper.getModifiedFiles(
-                                    mavenSession, mojoExecution,
-                                    maven, buildContext,
-                                    "schemaDirectory")))
-        {
+        if (Arrays.asList(
+                BuildHelper.getModifiedFiles(mavenSession, mojoExecution, maven, buildContext, "schemaDirectory"))
+                .isEmpty()) {
             return null;
         }
 
         final Set<IProject> result = super.build(kind, monitor);
 
-        File generated =
-                maven.getMojoParameterValue(
-                        getSession(),
-                        getMojoExecution(),
-                        "sourceGenerationDirectory",
-                        File.class);
-        if (generated != null)
-        {
-            buildContext.refresh(generated);
+        List<String> refresh = Arrays.asList("sourceGenerationDirectory", "classGenerationDirectory", "staleFile");
+        for (String param : refresh) {
+            File location = maven.getMojoParameterValue(projectFacade.getMavenProject(), getMojoExecution(), param,
+                    File.class, null);
+            if (location != null) {
+                buildContext.refresh(location);
+            }
         }
-
-        generated =
-                maven.getMojoParameterValue(
-                        getSession(),
-                        getMojoExecution(),
-                        "classGenerationDirectory",
-                        File.class);
-        if (generated != null)
-        {
-            buildContext.refresh(generated);
-        }
-
-        generated =
-                maven.getMojoParameterValue(
-                        getSession(),
-                        getMojoExecution(),
-                        "staleFile",
-                        File.class);
-        if (generated != null)
-        {
-            buildContext.refresh(generated);
-        }
-
 
         return result;
     }
--- xmlbeans-m2e-connector/org.bitstrings.eclipse.m2e.connectors.xmlbeans/src/org/bitstrings/eclipse/m2e/connectors/xmlbeans/XmlBeansProjectConfigurator.java
+++ xmlbeans-m2e-connector/org.bitstrings.eclipse.m2e.connectors.xmlbeans/src/org/bitstrings/eclipse/m2e/connectors/xmlbeans/XmlBeansProjectConfigurator.java
@@ -10,26 +10,89 @@
  */
 package org.bitstrings.eclipse.m2e.connectors.xmlbeans;
 
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecution;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
-import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+import org.eclipse.m2e.jdt.AbstractSourcesGenerationProjectConfigurator;
+import org.eclipse.m2e.jdt.IClasspathDescriptor;
 
-public class XmlBeansProjectConfigurator extends AbstractJavaProjectConfigurator
-{
+
+public class XmlBeansProjectConfigurator extends AbstractSourcesGenerationProjectConfigurator {
     @Override
-    protected String getOutputFolderParameterName()
-    {
+    protected String getOutputFolderParameterName() {
         return "sourceGenerationDirectory";
     }
 
     @Override
-    public AbstractBuildParticipant getBuildParticipant(
-                    IMavenProjectFacade projectFacade,
-                    MojoExecution execution,
-                    IPluginExecutionMetadata executionMetadata)
-    {
-        return new XmlBeansBuildParticipant(execution);
+    public void configure(ProjectConfigurationRequest request, IProgressMonitor monitor) throws CoreException {
+        super.configure(request, monitor);
+        for (MojoExecution mojoExecution : getMojoExecutions(request, monitor)) {
+            Object configuration = mojoExecution.getPlugin().getConfiguration();
+            Xpp3Dom conf = (Xpp3Dom) configuration;
+            // Force the noJavac option to 'true' Eclipse will compile the code
+            // so no point having XMLBeans do it.
+            Xpp3Dom noJavac = conf.getChild("noJavac");
+            if (noJavac == null) {
+                noJavac = new Xpp3Dom("noJavac");
+                conf.addChild(noJavac);
+            }
+            noJavac.setValue(Boolean.TRUE.toString());
+        }
+    }
+
+    /**
+     * Override the default, we want more control of the
+     * classpath.addSourceEntry operation.
+     */
+    @Override
+    protected File[] getSourceFolders(ProjectConfigurationRequest request, MojoExecution mojoExecution,
+            IProgressMonitor monitor) throws CoreException {
+        return new File[0];
+    }
+
+    /**
+     * Configure a source folder for both the generated sources and the
+     * generated resources that go in the 'class generation' directory (the all
+     * important schema information).
+     */
+    @Override
+    public void configureRawClasspath(ProjectConfigurationRequest request, IClasspathDescriptor classpath,
+            IProgressMonitor monitor) throws CoreException {
+        // Keep the same behaviour as the original.
+        super.configureRawClasspath(request, classpath, monitor);
+
+        IMavenProjectFacade facade = request.getMavenProjectFacade();
+        for (MojoExecution mojoExecution : getMojoExecutions(request, monitor)) {
+            File sourceDir = getParameterValue(request.getMavenProject(), getOutputFolderParameterName(), File.class,
+                    mojoExecution, monitor);
+            IPath sourcePath = getFullPath(facade, sourceDir);
+            if (sourcePath != null) {
+                classpath.addSourceEntry(sourcePath, facade.getOutputLocation(), true);
+            }
+
+            File genClassesDir = getParameterValue(request.getMavenProject(), "classGenerationDirectory", File.class,
+                    mojoExecution, monitor);
+            IPath genClassesPath = getFullPath(facade, genClassesDir);
+            if (genClassesPath != null) {
+                Path include = new Path("schemaorg_apache_xmlbeans/**");
+                classpath.addSourceEntry(genClassesPath, facade.getOutputLocation(), new IPath[] { include }, null,
+                        true);
+            }
+        }
+    }
+
+    @Override
+    public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution,
+            IPluginExecutionMetadata executionMetadata) {
+        return new XmlBeansBuildParticipant(projectFacade, execution);
     }
 }
